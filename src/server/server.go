32250331d5b29a84f71d88a40ba185435d2d9401
shaoying <shaoying@zuoyebang.com>
2020-08-28 20:49:01 +0800

    opt some

--- src/server/server.go
+++ src/server/server.go
@@ -3,99 +3,64 @@ package server
 import (
 	ice "github.com/shylinux/icebergs"
 	"github.com/shylinux/icebergs/base/cli"
-	"github.com/shylinux/icebergs/base/tcp"
 	"github.com/shylinux/icebergs/core/code"
 	kit "github.com/shylinux/toolkits"
 
 	"path"
 	"runtime"
-	"strings"
 )
 
-const MYSQL = "mysql"
-const _my_cnf = `
-[mysqld]
-basedir = ./
-datadir = ./data
-port = %s
-socket = ./mysqld.socket
-
-sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES 
-[mysqld_safe]
-log-error = ./mysqld.log
-pid-file = ./mysqld.pid
-`
+const (
+	MYSQL  = "mysql"
+	SERVER = "server"
+)
 
 var Index = &ice.Context{Name: MYSQL, Help: "mysql",
 	Configs: map[string]*ice.Config{
-		MYSQL: {Name: MYSQL, Help: "mysql", Value: kit.Data(
+		SERVER: {Name: SERVER, Help: "服务器", Value: kit.Data(
 			"windows", "https://mirrors.tuna.tsinghua.edu.cn/mysql/downloads/MySQL-5.6/mysql-5.6.48.zip",
 			"darwin", "https://mirrors.tuna.tsinghua.edu.cn/mysql/downloads/MySQL-5.6/mysql-5.6.48.tar.gz",
 			"linux", "https://mirrors.tuna.tsinghua.edu.cn/mysql/downloads/MySQL-5.6/mysql-5.6.48.tar.gz",
+
+			"build", []string{
+				"-DCMAKE_INSTALL_PREFIX=./install",
+				"-DDEFAULT_COLLATION=utf8_general_ci",
+				"-DDEFAULT_CHARSET=utf8",
+				"-DEXTRA_CHARSETS=all",
+			},
+			"start", []string{
+				"--basedir=./", "--datadir=./data", "--plugin-dir=./lib/plugin",
+				"--log-error=./mysqld.log", "--pid-file=./mysqld.pid",
+				"--socket=./mysqld.socket",
+			},
 		)},
 	},
 	Commands: map[string]*ice.Command{
 		ice.CTX_INIT: {Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) { m.Load() }},
 		ice.CTX_EXIT: {Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) { m.Save() }},
 
-		MYSQL: {Name: "mysql port=auto auto 启动:button 编译:button 下载:button cmd:textarea", Help: "mysql", Action: map[string]*ice.Action{
+		SERVER: {Name: "server port=auto path=auto auto 启动:button 构建:button 下载:button", Help: "服务器", Action: map[string]*ice.Action{
 			"download": {Name: "download", Help: "下载", Hand: func(m *ice.Message, arg ...string) {
-				m.Cmdy(code.INSTALL, "download", m.Conf(MYSQL, kit.Keys(kit.MDB_META, runtime.GOOS)))
+				m.Cmdy(code.INSTALL, "download", m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)))
 			}},
-			"compile": {Name: "compile", Help: "编译", Hand: func(m *ice.Message, arg ...string) {
-				name := path.Base(strings.TrimSuffix(strings.TrimSuffix(m.Conf(MYSQL, kit.Keys(kit.MDB_META, runtime.GOOS)), ".tar.gz"), "zip"))
-				m.Option(cli.CMD_DIR, path.Join(m.Conf(code.INSTALL, kit.META_PATH), name))
-				m.Cmdy(cli.SYSTEM, "cmake", "./", "-DCMAKE_INSTALL_PREFIX=./install",
-					"-DDEFAULT_COLLATION=utf8_general_ci", "-DDEFAULT_CHARSET=utf8",
-					"-DEXTRA_CHARSETS=all",
-				)
-
-				m.Cmdy(cli.SYSTEM, "make", "-j8")
-				m.Cmdy(cli.SYSTEM, "make", "install")
+			"build": {Name: "build", Help: "构建", Hand: func(m *ice.Message, arg ...string) {
+				m.Optionv("prepare", func(p string) {
+					m.Option(cli.CMD_DIR, p)
+					m.Cmdy(cli.SYSTEM, "cmake", "./", m.Confv(SERVER, "meta.build"))
+				})
+				m.Cmdy(code.INSTALL, "build", m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)))
 			}},
 			"start": {Name: "start", Help: "启动", Hand: func(m *ice.Message, arg ...string) {
-				// 分配
-				port := m.Cmdx(tcp.PORT, "select")
-				p := path.Join(m.Conf(cli.DAEMON, kit.META_PATH), port)
-
-				// 复制
-				name := path.Base(strings.TrimSuffix(strings.TrimSuffix(m.Conf(MYSQL, kit.Keys(kit.MDB_META, runtime.GOOS)), ".tar.gz"), "zip"))
-				from := kit.Path(path.Join(m.Conf(code.INSTALL, kit.META_PATH), name, "install"))
-				m.Cmdy(cli.SYSTEM, "cp", "-r", from, p)
-
-				// 生成
-				m.Option(cli.CMD_DIR, p)
-				m.Cmd(cli.SYSTEM, "./scripts/mysql_install_db", "--datadir=./data")
-				if f, _, e := kit.Create(path.Join(p, "my.cnf")); m.Assert(e) {
-					f.WriteString(kit.Format(_my_cnf, port))
-				}
-
-				// 启动
-				m.Option(cli.CMD_STDOUT, path.Join(p, "data/mysqld.log"))
-				m.Option(cli.CMD_STDERR, path.Join(p, "data/mysqld.log"))
-				m.Cmdy(cli.DAEMON, "bin/mysqld",
-					"--basedir=./", "--datadir=./data", "--plugin-dir=./lib/plugin",
-					"--log-error=./mysqld.log", "--pid-file=./mysqld.pid",
-					"--socket=./mysqld.socket", "--port="+port)
+				m.Optionv("prepare", func(p string) []string {
+					m.Option(cli.CMD_DIR, p)
+					m.Cmd(cli.SYSTEM, "./scripts/mysql_install_db", "--datadir=./data")
+					return []string{"--port", path.Base(p)}
+				})
+				m.Cmdy(code.INSTALL, "start", m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS)),
+					"bin/mysqld", m.Confv(SERVER, "meta.start"))
 			}},
 		}, Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {
-			if len(arg) > 0 && arg[0] != "" {
-				m.Option(cli.CMD_DIR, path.Join("var/daemon", arg[0]))
-				m.Cmdy(cli.SYSTEM, "bin/mysql", "-S", "data/mysqld.socket", "-u", "root", "-e", kit.Select("show databases", arg, 1))
-				return
-			}
-
-			m.Cmd(cli.DAEMON).Table(func(index int, value map[string]string, head []string) {
-				if strings.HasPrefix(value[kit.MDB_NAME], "bin/mysql") {
-					m.Push(kit.MDB_TIME, value[kit.MDB_TIME])
-					m.Push(kit.MDB_PID, value[kit.MDB_PID])
-					m.Push(kit.MDB_DIR, value[kit.MDB_DIR])
-					m.Push(kit.MDB_PORT, path.Base(value[kit.MDB_DIR]))
-					m.Push(kit.MDB_STATUS, value[kit.MDB_STATUS])
-					m.Push(kit.MDB_NAME, value[kit.MDB_NAME])
-				}
-			})
-			m.Sort("time", "time_r")
+			m.Cmdy(code.INSTALL, path.Base(m.Conf(SERVER, kit.Keys(kit.MDB_META, runtime.GOOS))), arg)
 		}},
 	},
 }
