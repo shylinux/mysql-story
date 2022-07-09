bac1a618acd57c94c92edf79cebfc85e3713d908
harveyshao <harveyshao@tencent.com>
2022-07-09 20:13:38 +0800

    opt css

--- src/search/es.go
+++ src/search/es.go
@@ -26,7 +26,7 @@ var Index = &ice.Context{Name: ES, Help: "搜索",
 		ice.CTX_INIT: {Hand: func(m *ice.Message, arg ...string) {}},
 		ice.CTX_EXIT: {Hand: func(m *ice.Message, arg ...string) {}},
 
-		ES: {Name: "es port=auto path=auto auto 启动:button 下载", Help: "搜索", Action: map[string]*ice.Action{
+		ES: {Name: "es port=auto path=auto auto 启动:button 下载", Help: "搜索", Actions: map[string]*ice.Action{
 			"download": {Name: "download", Help: "下载", Hand: func(m *ice.Message, arg ...string) {
 				m.Cmdy(code.INSTALL, "download", m.Conf(ES, kit.Keys(kit.MDB_META, runtime.GOOS)))
 			}},
@@ -43,7 +43,7 @@ var Index = &ice.Context{Name: ES, Help: "搜索",
 			m.Cmdy(code.INSTALL, name, arg)
 		}},
 
-		"GET": {Name: "GET 查看:button cmd:text=/", Help: "命令", Action: map[string]*ice.Action{}, Hand: func(m *ice.Message, arg ...string) {
+		"GET": {Name: "GET 查看:button cmd:text=/", Help: "命令", Actions: map[string]*ice.Action{}, Hand: func(m *ice.Message, arg ...string) {
 			if pod := m.Option("_pod"); pod != "" {
 				m.Option("_pod", "")
 				m.Cmdy(web.SPACE, pod, m.PrefixKey(), arg)
@@ -58,7 +58,7 @@ var Index = &ice.Context{Name: ES, Help: "搜索",
 			m.Echo(kit.Formats(kit.UnMarshal(m.Cmdx(web.SPIDE, ice.DEV, web.SPIDE_RAW,
 				web.SPIDE_GET, kit.MergeURL2(m.Conf(ES, "meta.address"), kit.Select("/", arg, 0))))))
 		}},
-		"CMD": {Name: "CMD 执行:button method:select=GET|PUT|POST|DELETE cmd:text=/ data:textarea", Help: "命令", Action: map[string]*ice.Action{}, Hand: func(m *ice.Message, arg ...string) {
+		"CMD": {Name: "CMD 执行:button method:select=GET|PUT|POST|DELETE cmd:text=/ data:textarea", Help: "命令", Actions: map[string]*ice.Action{}, Hand: func(m *ice.Message, arg ...string) {
 			if pod := m.Option("_pod"); pod != "" {
 				m.Option("_pod", "")
 				m.Cmdy(web.SPACE, pod, m.PrefixKey(), arg)
