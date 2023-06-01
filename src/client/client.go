545fd4c92f113d10902178af1e61ca0097f3bf5a
shylinux <shylinux@163.com>
2023-06-01 09:57:18 +0800

    opt some

--- src/client/client.go
+++ src/client/client.go
@@ -26,7 +26,7 @@ type Client struct {
 	short string `data:"sess"`
 	field string `data:"time,sess,username,host,port,database"`
 
-	create string `name:"create sess*=biz username*=root password*=root host*=127.0.0.1 port*=10002 database*=mysql" help:"连接"`
+	create string `name:"create sess*=biz username*=root password*=root host*=127.0.0.1 port*=10001 database*=mysql" help:"连接"`
 	list   string `name:"list sess@key database@key auto create stmt:textarea" help:"存储"`
 }
 
