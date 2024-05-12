2d94427685a755289ee6969bdd28a7c4b92de12f
shy <shy@shylinux.com>
2024-05-12 23:39:15 +0800

    add some

--- src/main.go
+++ src/main.go
@@ -4,7 +4,6 @@ import (
 	"shylinux.com/x/ice"
 	_ "shylinux.com/x/mysql-story/src/client"
 	_ "shylinux.com/x/mysql-story/src/server"
-	_ "shylinux.com/x/mysql-story/src/sqlite"
 	_ "shylinux.com/x/mysql-story/src/studio"
 )
 
